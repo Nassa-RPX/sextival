1f269f8554f443619f84e9728b3de48dabdf0780
Giovanni Costa <cgiovanni1996@gmail.com>
2022-08-26 17:25:28 +0200

    :feat: hero tagline

--- src/components/hero/hero.component.tsx
+++ src/components/hero/hero.component.tsx
@@ -1,3 +1,4 @@
+import { BaseProps } from "@sextival/types/react.types";
 import Image from "next/image";
 import { Panel, Spacer } from "../base";
 
@@ -17,11 +18,34 @@ const Hero = () => {
       <HeroTitle text="INTERVENTI" />
       <HeroTitle text="ARTE" />
       <HeroTitle text="MUSICA" />
+
+      <Spacer type="vertical" dimension="small" />
+
+      <HeroTagline color={"text-gray-800"}>VIENI</HeroTagline>
+      <HeroTagline color={"text-white"} leading="leading-none">
+        AL FESTIVAL <br /> DELL&apos;EDUCAZIONE <br />
+        SESSUALE
+      </HeroTagline>
     </Panel>
   );
 };
 
 const HeroTitle = ({ text }: { text: string }) => (
-  <h1 className="text-white font-bold text-5xl lg:text-6xl">{text}</h1>
+  <h1 className="text-white font-bold text-5xl lg:text-6xl drop-shadow-lg">
+    {text}
+  </h1>
+);
+
+interface HeroTaglineProps extends BaseProps {
+  color: string;
+  leading?: string;
+}
+
+const HeroTagline = ({ color, leading, children }: HeroTaglineProps) => (
+  <h2
+    className={`text-right font-bold text-xl lg:text-3xl ${color} ${leading} drop-shadow-md`}
+  >
+    {children}
+  </h2>
 );
 export default Hero;
