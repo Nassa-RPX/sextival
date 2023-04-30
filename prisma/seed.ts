d983d3f070b7e99810fb1adb6c6818b24e479a29
Giovanni Costa <cgiovanni1996@gmail.com>
2023-04-30 18:10:13 +0200

    :chore: removed data seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,23 +1,23 @@
 import { PrismaClient } from "@prisma/client";
-import { guests } from "./data";
+// import { guests } from "./data";
 const prisma = new PrismaClient();
 
 export const load = async () => {
   try {
-    await prisma.user.deleteMany();
-    console.log("Deleted records in user table");
-    await prisma.guest.deleteMany();
-    console.log("Deleted records in guest table");
+    //
+    // await prisma.user.deleteMany();
+    // console.log("Deleted records in user table");
+    // await prisma.guest.deleteMany();
+    // console.log("Deleted records in guest table");
 
     // await prisma.user.createMany({
     //   data: categories,
     // });
     // console.log("Added user data");
 
-    await prisma.guest.createMany({
-      data: guests,
-    });
-    console.log("Added guest data");
+    // await prisma.guest.createMany({
+    //   data: guests,
+    // });
   } catch (e) {
     console.error(e);
     process.exit(1);
