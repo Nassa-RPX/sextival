61db102c4329913a3e12b5004b598965a0ffae14
Giovanni Costa <cgiovanni1996@gmail.com>
2023-06-01 12:01:57 +0200

    :bug: cassetta/servizi /3

--- src/ui/markdown/markdown.ui.tsx
+++ src/ui/markdown/markdown.ui.tsx
@@ -1,5 +1,6 @@
 // import { PropsWithChildren } from "react";
 import { LinkIcon } from "@sextival/components/icon";
+import clsx from "clsx";
 import ReactMarkdown from "react-markdown";
 import remarkGfm from "remark-gfm";
 
@@ -12,7 +13,7 @@ export const Markdown = ({ content }: Props) => (
     remarkPlugins={[remarkGfm]}
     components={{
       h3: (options) => {
-        return <h3 className="text-2xl">{options.children}</h3>;
+        return <h3 className="mt-4 text-2xl">{options.children}</h3>;
       },
       ul: (options) => {
         return (
@@ -24,6 +25,30 @@ export const Markdown = ({ content }: Props) => (
           <a className="flex items-center font-bold gap-2" href={options.href}>
             <span>Clicca qui</span> <LinkIcon />
           </a>
+        );
+      },
+      li: (options) => {
+        console.log("options", options);
+
+        return (
+          <>
+            {Array.isArray(options.children)
+              ? (
+                <li
+                  className={clsx(
+                    "mt-2",
+                    options.children.filter((c) => typeof c !== "string")
+                        .length >
+                        1
+                      ? "[&>p]:inline-block"
+                      : "[&>p]:inline",
+                  )}
+                >
+                  {options.children}
+                </li>
+              )
+              : <li>{options.children}</li>}
+          </>
         );
       },
     }}
